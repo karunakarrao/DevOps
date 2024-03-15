b6abbcceae7761849d6949a66a43a3160fb53678
Karunakar Rao J <33980623+karunakarrao@users.noreply.github.com>
2024-03-15 21:29:12 +0530

    Delete Terraform/Lab-1  Remote-state backend setup using S3 and Dynamodb directory

--- Terraform/Lab-1  Remote-state backend setup using S3 and Dynamodb/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "karna-test-s3-3-15-2024"       # s3 must create first 
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_state_lock"
-    encrypt        = true
-  }
-}
